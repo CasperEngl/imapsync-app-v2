661073012603f7759a2b8ae7888a62d9a1241531
Casper Engelmann <me@casperengelmann.com>
2025-01-23 23:27:48 +0100

    Refactor progress component and add animation to the progress barRefactor progress component and add animation to the progress bar

--- src/renderer/components/ui/progress.tsx
+++ src/renderer/components/ui/progress.tsx
@@ -1,6 +1,6 @@
-import * as ProgressPrimitive from "@radix-ui/react-progress"
-import * as React from "react"
-import { cn } from '~/renderer/lib/utils.js'
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import * as React from "react";
+import { cn } from "~/renderer/lib/utils.js";
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
@@ -17,12 +17,17 @@ const Progress = React.forwardRef<
     <ProgressPrimitive.Indicator
       className={cn(
         "h-full w-full flex-1 transition-all",
-        value && value > 0 ? "bg-primary" : "bg-muted"
+        value
+          ? [
+              value > 0 && value < 100 ? "animate-pulse" : null,
+              value > 0 ? "bg-primary" : "bg-muted",
+            ]
+          : null
       )}
-      style={{ transform: `translateX(-${ 100 - (value || 0) }%)` }}
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
 
-export { Progress }
+export { Progress };
